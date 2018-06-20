751f9622a0bef8c06faebed12fffb6a6c446edfe
ddy250 <odedsagi@gmail.com>
2018-06-20 11:12:07 +0300

    last version

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,37 @@ var router = express.Router();
 var rp = require('request-promise');
 var request = require('request');
 
+var d=[];
+var x;
+function kFormatter(num,newarray) {
+    for (x in num) {
+        if (num[x].companyRevenue > 999999) {
+            newarray[x] = (num[x].companyRevenue / 1000000).toFixed(2)+'M';
+            newarray[x] = '$' + newarray[x];
+        }
+        else if (num[x].companyRevenue > 999) {
+            newarray[x] = (num[x].companyRevenue / 1000).toFixed(2)+'k';
+            newarray[x] = '$' + newarray[x];
+            console.log(num[x]);
+        }
+        else if (num[x].companyRevenue < 999) {
+            newarray[x] = num[x].companyRevenue;
+            newarray[x] = '$' + newarray[x];
+        }
+    }
+    return newarray;
+}
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     request('https://evening-dawn-29014.herokuapp.com/list', function (error, response, body) {
-        var c= JSON.parse(body);
+        var c = JSON.parse(body);
+        kFormatter(c, d);
         res.render('index', {
-            c: c
+            c: c,
+            d:d
         });
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
     });
-
 });
 
 module.exports = router;
--- routes/list.js
+++ routes/list.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function (req, res, next) {
     res.send(
-            [
-                {
+        [
+            {
                 "profileImageURL": "http://www.roughbros.com/assets/uploads/leadership/dave.jpg",
                 "industry": [
                     "Software",
@@ -13,6 +13,8 @@ router.get('/', function (req, res, next) {
                     "Retail",
                     "Business Services"
                 ],
+                "companyCity": "Houston",
+                "companyCountry": "U.S.A",
                 "lastUpdatedDate": "2017-03-13",
                 "name": "David Roberts",
                 "title": "Federal Inside Sales District Manager",
@@ -22,28 +24,32 @@ router.get('/', function (req, res, next) {
                 "companyDomain": "www.insight.com",
                 "id": 1,
                 "email": "personmail@gmail.com",
-                "phone": "05083333333"
+                "phone": "05083333333",
+                "companyRevenue": 2555333
             },
-                {
-                    "profileImageURL": "http://www.roughbros.com/assets/uploads/leadership/dave.jpg",
-                    "industry": [
-                        "Soft",
-                        "Manufacturing",
-                        "Retail",
-                        "Business Services"
-                    ],
-                    "lastUpdatedDate": "2017-03-13",
-                    "name": "David Roberts",
-                    "title": "Federal Inside Sales District Manager",
-                    "companyLogo": "http://img2.insight.com/graphics/insight/en-gb/content/header_black_logo.gif",
-                    "companyName": "Insight",
-                    "companyEmployees": 5761,
-                    "companyDomain": "www.insight.com",
-                    "id": 1,
-                    "email": "personmail@gmail.com",
-                    "phone": "05083333333"
-                }
-            ]
+            {
+                "profileImageURL": "http://www.roughbros.com/assets/uploads/leadership/dave.jpg",
+                "industry": [
+                    "Soft",
+                    "Manufacturing",
+                    "Retail",
+                    "Business Services"
+                ],
+                "companyCity": "Houston",
+                "companyCountry": "U.S.A",
+                "lastUpdatedDate": "2017-03-13",
+                "name": "David Roberts",
+                "title": "Federal Inside Sales District Manager",
+                "companyLogo": "http://img2.insight.com/graphics/insight/en-gb/content/header_black_logo.gif",
+                "companyName": "Insight",
+                "companyEmployees": 5761,
+                "companyDomain": "www.insight.com",
+                "id": 1,
+                "email": "personmail@gmail.com",
+                "phone": "05083333333",
+                "companyRevenue": 1930
+            }
+        ]
     );
 });
 
